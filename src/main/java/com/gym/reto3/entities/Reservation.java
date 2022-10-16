5aaf757aaab4cc147db7d900abe0f405f55a0dec
Esteban-DdS <ztheeban205@gmail.com>
2022-10-16 13:43:38 -0500

    Entities completas

---
+++ src/main/java/com/gym/reto3/entities/Reservation.java
@@ -0,0 +1,78 @@
+package com.gym.reto3.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="reservation")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idReservation;
+    private Date startDate;
+    private String status="created";
+
+    @ManyToOne
+    @JoinColumn(name = "machineId")
+    @JsonIgnoreProperties("reservations")
+    private Machine machine;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId")
+    @JsonIgnoreProperties({"reservations", "messages"})
+    private Client client;
+
+    @OneToMany(cascade = {CascadeType.REMOVE}, mappedBy = "reservation")
+    @JsonIgnoreProperties("reservation")
+    private Score score;
+
+    public Integer getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(Integer idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Machine getMachine() {
+        return machine;
+    }
+
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+}
