dac14c1c43fa03ce63e2799664dd8a0005437e5f
Esteban-DdS <ztheeban205@gmail.com>
2022-10-16 13:11:59 -0500

    repositorios Category y Machine

---
+++ src/main/java/com/gym/reto3/repository/crudrepository/CategoryCrudRepository.java
@@ -0,0 +1,7 @@
+package com.gym.reto3.repository.crudrepository;
+
+import com.gym.reto3.entities.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryCrudRepository extends CrudRepository<Category, Integer> {
+}
---
+++ src/main/java/com/gym/reto3/repository/crudrepository/MachineCrudRepository.java
@@ -0,0 +1,7 @@
+package com.gym.reto3.repository.crudrepository;
+
+import com.gym.reto3.entities.Machine;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MachineCrudRepository extends CrudRepository<Machine, Integer> {
+}
